935501f92b9d8f508f07cae789e9a772d8ca93ec
Devan Stormont <stormont@gmail.com>
2012-05-26 15:56:40 -0700

    Updated gitsvc to not use command line args

--- gitsvc.hs
+++ gitsvc.hs
@@ -7,9 +7,11 @@ import Control.Monad.Maybe (MaybeT(..), runMaybeT)
 import Control.Monad.Trans (liftIO)
 import Data.Time
 import System.Exit (ExitCode (ExitSuccess))
+import System.IO (hFlush, stdout)
 import System.Process (system)
 
 
+main :: IO ()
 main = do
    startTime <- getCurrentTime
    r <- runMaybeT $ runGitSVC
@@ -23,36 +25,26 @@ main = do
 
 runGitSVC :: MaybeT IO ()
 runGitSVC = do
+   liftIO $ putStr "Enter git add options to use [defaults to -v -A]: "
+   liftIO $ hFlush stdout
    options <- liftIO $ getOptions
-   filematch <- liftIO $ getFilematch
-   opts <- if options == []
-      then return ("-v -A")
-      else return options
+   liftIO $ putStr "Enter git add filematch to use: "
+   liftIO $ hFlush stdout
+   filematch <- liftIO $ getLine
    execProc $ "git add " ++ options ++ " *" ++ filematch
-   comment <- liftIO $ getComment
+   liftIO $ putStr "Enter git commit comment to use: "
+   liftIO $ hFlush stdout
+   comment <- liftIO $ getLine
    execProc $ "git commit -m \"" ++ comment ++ "\""
-   --execProc $ "git push -u origin master"
+   execProc $ "git push -u origin master"
 
 
 getOptions :: IO (String)
 getOptions = do
-   putStr "Enter git add options to use [defaults to -v -A]: "
    input <- getLine
    if input == []
      then return ("-v -A")
      else return (input)
-
-
-getFilematch :: IO (String)
-getFilematch = do
-   putStr "Enter git add filematch to use: "
-   getLine
-
-
-getComment :: IO (String)
-getComment = do
-   putStr "Enter git commit comment to use: "
-   getLine
 
 
 execProc :: String -> MaybeT IO ()
